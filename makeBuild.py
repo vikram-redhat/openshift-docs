7b1ba861d10fe1016400fc86d3b7ce7ff3e67447
Vikram Goyal <vigoyal@redhat.com>
2020-11-25 19:37:01 +1000

    Update makeBuild.py

--- makeBuild.py
+++ makeBuild.py
@@ -9,7 +9,7 @@ import logging
 import imp
 imp.reload(sys)
 
-from lxml.etree import XMLSyntaxError, XIncludeError
+from xml.etree import XMLSyntaxError, XIncludeError
 
 from aura import cli, utils
 from aura.exceptions import InvalidInputException
